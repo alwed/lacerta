d37fa15d469a43599702199d102a65e7c11b3abf
Felix Knopf <dl2ik@darc.de>
2023-05-17 23:29:04 +0200

    rx: Improve SDR chain

--- script/rx.sh
+++ script/rx.sh
@@ -5,7 +5,7 @@ mydir="$(realpath $(dirname $0))"
 PATH=$PATH:"$mydir":"$mydir/../build"
 
 # Callsign or listener name
-MYCALL=DL2IK
+MYCALL=
 
 # Disable as desired
 sondehub.py &
@@ -13,13 +13,16 @@ ssdv_upload.py &
 
 # Adjust frequency (f_c - 100kHz) to payload
 # fskdemodgui can be disabled, eg. on headless systems
-rtl_sdr -f 434000000 -s 2400000 -g 5 - \
+rtl_sdr -f 433900000 -s 2400000 -g 0 - \
 	| csdr convert_u8_f |  \
-	csdr fir_decimate_cc 6 .05 HAMMING | \
-	csdr bandpass_fir_fft_cc -0.1 0.45 0.05 | csdr convert_f_s16 | \
-	fsk_demod -c -s -t2 2 400000 50000 - - \
-	2> >(fskdemodgui.py 2>/dev/null) | frame-dec | ssdv_split | \
-	telemetry-broadcast.py
+	csdr bandpass_fir_fft_cc 0.02 0.1666 0.02 | \
+	csdr fir_decimate_cc 3 .005 HAMMING | \
+	csdr realpart_cf | csdr gain_ff 0.5 | \
+	csdr convert_f_s16 | \
+	fsk_demod -s -t2 2 800000 50000 - - \
+	2> >(fskdemodgui.py --wide 2>/dev/null) | frame-dec \
+	| ssdv_split | telemetry-broadcast.py
 
-jobs -p | xargs kill
+
+jobs -rp | xargs kill
 wait
